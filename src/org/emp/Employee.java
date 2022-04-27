6bab6ddce17915005ad9332d7fb13cce90887a97
Ragavi5 <ragavi.ravindran5@gmail.com>
2022-04-27 15:49:11 +0530

    test

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,30 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("Employee ID 56582");
+
+	}
+ private void empId(String name) {
+	 System.out.println("Employee Name:"+name);
+}
+ private void empId(int age) {
+	 System.out.println("Employee Age:"+age);
+ }
+ private void empId(int pin, float height) {
+	 System.out.println("Employee Pin:"+pin+"\n"+"Employee height:"+height);
+}
+ private void empId(float weight) {
+	 System.out.println("Employee weight:"+weight);	
+}
+ public static void main(String[] args) {
+	Employee e = new Employee();
+	e.empId();
+	e.empId("Ragavi");
+	e.empId(24);
+	e.empId(620002, 5.2f);
+	e.empId(63f);
+}
+	
+
+}
